00b4fcfbae5c33a71fd2dde269cae452d9411e99
Julia <juicy.jelly74@gmail.com>
2016-05-07 12:06:05 +0300

    part 2

---
+++ lab7_2/lab7_2/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  lab7_2
+//
+//  Created by Admin on 06.05.16.
+//  Copyright © 2016 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface GameViewController : GLKViewController
+
+@end
---
+++ lab7_2/lab7_2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  lab7_2
+//
+//  Created by Admin on 06.05.16.
+//  Copyright © 2016 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
